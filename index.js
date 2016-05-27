c5307fb1d4bb8c1ad3fd99859ad03f58fea4ce97
bryantbello <bryant.bello@outlook.com>
2016-05-26 21:22:19 -0400

    cheerio

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var request = require('request');
+var cheerio = require('cheerio');​
+//Database configuration
+var mongojs = require('mongojs');
+var databaseUrl = "scraper";
+var collections = ["scrapedData"];
+var db = mongojs(databaseUrl, collections);
+db.on('error', function(err) {
+    console.log('Database Error:', err);
+});​​​
+app.get("/", function(req, res) {
+    request('https://news.ycombinator.com/', function(error, response, html) {
+        var $ = cheerio.load(html);
+        //console.log(response);
+        $('.title').each(function(index) {
+            var title = $(this).children('a').text();
+            var link = $(this).children('a').attr('href');
+            console.log(title);
+            console.log(link);
+            db.scrapedData.save({
+                title: title,
+                link: link
+            });
+        });​​
+    });
+
+    ​
+});
+app.listen(3000, function() {
+    console.log('App running on port 3000!');
+});
